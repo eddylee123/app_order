f0deeb4e0ebe37e60aa13500738b13514e2f57c9
lwz <786838216@qq.com>
2024-04-16 22:03:18 +0800

    mq test2

--- public/queue.php
+++ public/queue.php
@@ -21,7 +21,6 @@ if(!extension_loaded('posix'))
 }
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
-define('ROOT_PATH', dirname(realpath(APP_PATH)) . '/');
 // 绑定模块
 define('BIND_MODULE','gateway/Startqueue');
 // 加载框架引导文件
